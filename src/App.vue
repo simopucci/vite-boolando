baf59aaa6ae762508cd088a72eba2c5181e10a43
simopucci <simo.pucci7@gmail.com>
2024-02-08 18:51:31 +0100

    feat: add components & AppHeader vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
 
+  export default {
+    data() {
+      return {
+
+      };
+    },
+
+    components: { AppHeader, AppMain, AppFooter }, 
+  };
 </script>
 
 <template>
-  
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,67 @@
+<script>
+    export default {
+        data() {
+            return {
+                logoImage: 'boolean-logo.png',
+
+                links: [
+                    {
+                        url: 'Uomo',
+                    },
+                    {
+                        url: 'Donna',
+                    },
+                    {
+                        url: 'Bambino',
+                    }
+                ],
+
+                icons: [
+                    {
+                        iconName: 'fa-regular fa-user',
+                    },
+                    {
+                        iconName: 'fa-regular fa-heart',
+                    },
+                    {
+                        iconName: 'fa-solid fa-bag-shopping',
+                    },
+                ],
+
+            };
+        },
+
+        methods: {
+            logoPath(logoName) {
+                const logoUrl = new URL("../assets/img/" + logoName, import.meta.url);
+                return logoUrl.href;
+            },
+        }
+    }
+</script>
+
+<template>
+    <header class="header-container">
+
+        <nav class="header-nav">
+            <div class="catalog-box">
+                <div v-for="link in links">
+                    <a href="#">{{ link.url }}</a>
+                </div>
+            </div>
+
+            <div class="header-logo">
+                <img :src="logoPath(logoImage)" alt="Logo Boolean">
+            </div>
+
+            <div class="header-profile">
+                <div v-for="icon in icons">
+                    <a href="#"><i :class="icon.iconName"></i></a>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+</template>
+
+<style></style>
